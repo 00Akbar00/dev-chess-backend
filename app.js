72ac6f1256935e65dd7930a34b035dd06d234898
00Akbar00 <ma341604@gmail.com>
2025-04-08 21:08:23 +0500

    Made database connection with mongoDB and installed nodemon

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { connectDB } = require("./config/db");
+const  connectDB  = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const apiRoutes = require('./routes');
